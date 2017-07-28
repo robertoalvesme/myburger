8920e6b1249e9644482a95a35bdca6732b6d720d
Roberto Fonseca <roberto@rhfactor.com.br>
2017-07-28 01:11:13 -0300

    Mapear ingredient

--- src/main/java/br/com/rhfactor/myburger/model/Ingredient.java
+++ src/main/java/br/com/rhfactor/myburger/model/Ingredient.java
@@ -2,6 +2,26 @@ package br.com.rhfactor.myburger.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+@Table(name = "ingredient")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(discriminatorType = DiscriminatorType.STRING, columnDefinition = "VARCHAR(3) NOT NULL", length = 3, name = "type")
+@DiscriminatorValue(value = "ING")
 public class Ingredient implements IIngredient {
 
 	public Ingredient() {
@@ -19,11 +39,19 @@ public class Ingredient implements IIngredient {
 		this.value = value;
 	}
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected Integer id;
 
+	@NotNull
+	@NotEmpty
+	@Column(length = 100, nullable = false, unique = true)
 	protected String name;
 
 	protected BigDecimal value;
+
+	@Column(insertable = false, updatable = false)
+	private String type;
 
 	public Integer getId() {
 		return id;
@@ -47,6 +75,14 @@ public class Ingredient implements IIngredient {
 
 	public void setValue(BigDecimal value) {
 		this.value = value;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 
 }
